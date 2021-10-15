de05eb96ac7d2075a82bea6a68a61a6a4bfa4253
martinLucidarme <martinlucidarme@gmail.com>
2021-10-15 13:58:45 -0300

    column names ok

--- test_create_txt.py
+++ test_create_txt.py
@@ -1,13 +1,16 @@
-class CriarTabela():
+class CriarTabela:
 
+    def __init__(self):
+        self.arquivo =0
 
-    def definir_colunas(lista_das_colunas):
+    def definir_colunas(self,lista_das_colunas):
         text_opening = open('text_test.txt','w') #file type object
         '''print(text_opening.read()) #opening.read is str type, this str is the text of the whole file'''
         for col in lista_das_colunas:
             text_opening.write(f'{col}'+'   ')
 
-lista_das_colunas = ['Lat', 'Long', 'N', 'P', 'K']
-CriarTabela.definir_colunas(lista_das_colunas)
+tableau = CriarTabela()
+lista_das_colunas = ['Lat', 'Long', 'N', 'P', 'K','Micro', 'pH', 'CTC','V%']
+tableau.definir_colunas(lista_das_colunas)
 
 
