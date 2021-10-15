e489c434af2be21614cc78e2e5b100e763f9a198
martinLucidarme <martinlucidarme@gmail.com>
2021-10-15 15:07:25 -0300

    column names ok

--- test_create_txt.py
+++ test_create_txt.py
@@ -4,6 +4,13 @@ class CriarTabela:
         self.arquivo =0
 
     def definir_colunas(self,lista_das_colunas):
+        '''
+        Give a name to every column: each str of the list will be the header of
+        the column
+        :param lista_das_colunas: list of str, each str is the name of a column
+        :return: write in a text file the name of the column
+        '''
+
         text_opening = open('text_test.txt','w') #file type object
         '''print(text_opening.read()) #opening.read is str type, this str is the text of the whole file'''
         for col in lista_das_colunas:
