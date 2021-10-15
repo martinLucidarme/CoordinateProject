c7899d4aa325a988f60a2cc11204d4bcc75f8c06
martinLucidarme <martinlucidarme@gmail.com>
2021-10-15 16:13:22 -0300

    column names ok

--- test_create_txt.py
+++ test_create_txt.py
@@ -1,7 +1,7 @@
+from create_coordinates import Coord_Clean
+
 class CriarTabela:
 
-    def __init__(self):
-        self.arquivo =0
 
     def definir_colunas(self,lista_das_colunas):
         '''
@@ -12,11 +12,14 @@ class CriarTabela:
         '''
 
         text_opening = open('text_test.txt','w') #file type object
-        '''print(text_opening.read()) #opening.read is str type, this str is the text of the whole file'''
+        #print(text_opening.read()) #opening.read is str type, this str is the text of the whole file
         for col in lista_das_colunas:
             text_opening.write(f'{col}'+'   ')
 
 tableau = CriarTabela()
+coord_clean= Coord_Clean()
+coord_clean.calc_coord()
+listcoord= coord_clean.calc_coord()
 lista_das_colunas = ['Lat', 'Long', 'N', 'P', 'K','Micro', 'pH', 'CTC','V%']
 tableau.definir_colunas(lista_das_colunas)
 
